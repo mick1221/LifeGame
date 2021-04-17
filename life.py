64f3619acec711b927823a5e2912ad77d37d0863
Noah Mickus <noah.mickus@gmail.com>
2021-04-16 19:28:37 -0500

    added dead state method

--- life.py
+++ life.py
@@ -1,5 +1,19 @@
 
 
 
+def dead_state(width, height):
+    life_space=[]
 
-def dead_state(width, height)
+    for i in range(height):
+        n = []
+        life_space.append(n)
+        for j in range(width):
+            
+            life_space[i].append(0)
+    
+    return life_space
+
+width = 4
+height = 5
+
+print(dead_state(width,height))
