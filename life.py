4b9e479ba7ecd1af47a3495d88baec362ccc317c
Noah Mickus <noah.mickus@gmail.com>
2021-04-19 16:36:41 -0500

    added a random state genrator

--- life.py
+++ life.py
@@ -1,8 +1,9 @@
 
+import random
 
 
 def dead_state(width, height):
-    life_space=[]
+    life_space = []
 
     for i in range(height):
         n = []
@@ -13,7 +14,28 @@ def dead_state(width, height):
     
     return life_space
 
+def random_state(width,height):
+    life_space=[]
+
+    for i in range(height):
+        n = []
+        life_space.append(n)
+        for j in range(width):
+
+            life = random.random()
+            if life >= 0.5:
+                state = 1
+            else:
+                state = 0
+            life_space[i].append(state)
+    
+    return life_space
+
+
+
+
 width = 4
 height = 5
 
 print(dead_state(width,height))
+print(random_state(width,height))
