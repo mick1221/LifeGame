5a110dd2fba3a561cc081e89b3d860cc939d09da
Noah Mickus <noah.mickus@gmail.com>
2021-04-21 10:53:02 -0500

    added a method to generate the next life board state

--- life.py
+++ life.py
@@ -31,6 +31,165 @@ def random_state(width,height):
     
     return life_space
 
+
+def next_state(life_space):
+    new_state = []
+
+    for i in range(len(life_space)-1):
+        t = []
+        new_state.append(t)
+        count = 0
+
+        # checking the top row
+        if i == 0:
+            for j in range(len(life_space[i])-1):
+                count = 0
+                if j == 0:
+                    if life_space[i][j+1] == 1:
+                        count +=1
+                    if life_space[i+1][j+1] == 1:
+                        count +=1
+                    if life_space[i+1][j] == 1:
+                        count +=1
+                    
+                elif j == len(life_space[i]):
+                    if life_space[i][j-1] == 1:
+                        count +=1
+                    if life_space[i+1][j-1] == 1:
+                        count +=1
+                    if life_space[i+1][j] == 1:
+                        count +=1
+                else:
+                    if life_space[i][j-1] == 1:
+                        count +=1
+                    if life_space[i+1][j-1] == 1:
+                        count +=1
+                    if life_space[i+1][j] == 1:
+                        count +=1
+                    if life_space[i][j+1] == 1:
+                        count +=1
+                    if life_space[i+1][j+1] == 1:
+                        count +=1
+                #checking for new life value
+                if life_space[i][j] == 0:
+                    if count == 3:
+                        new_state[i].append(1)
+                    else:
+                        new_state[i].append(0)
+                else:
+                    if count < 2:
+                        new_state[i].append(0)
+                    elif count > 3:
+                        new_state[i].append(0)
+                    else:
+                        new_state[i].append(1)
+        #checking bottom row
+        elif i == len(life_space):
+            for j in range(len(life_space[i])-1):
+                count = 0 
+                if j == 0:
+                    if life_space[i][j+1] == 1:
+                        count +=1
+                    if life_space[i-1][j+1] == 1:
+                        count +=1
+                    if life_space[i-1][j] == 1:
+                        count +=1
+                elif j == len(life_space[i])-1:
+                    if life_space[i][j-1] == 1:
+                        count +=1
+                    if life_space[i-1][j-1] == 1:
+                        count +=1
+                    if life_space[i-1][j] == 1:
+                        count +=1
+
+                else:
+                    if life_space[i][j-1] == 1:
+                        count +=1
+                    if life_space[i-1][j-1] == 1:
+                        count +=1
+                    if life_space[i-1][j] == 1:
+                        count +=1
+                    if life_space[i][j+1] == 1:
+                        count +=1
+                    if life_space[i-1][j+1] == 1:
+                        count +=1
+                
+                #checking for new life value
+                if life_space[i][j] == 0:
+                    if count == 3:
+                        new_state[i].append(1)
+                    else:
+                        new_state[i].append(0)
+                else:
+                    if count < 2:
+                        new_state[i].append(0)
+                    elif count > 3:
+                        new_state[i].append(0)
+                    else:
+                        new_state[i].append(1)
+        #checking middle rows
+        else:
+            for j in range(len(life_space[i])-1):
+                count = 0
+                if j == 0:
+                    if life_space[i][j+1] == 1:
+                        count +=1
+                    if life_space[i-1][j+1] == 1:
+                        count +=1
+                    if life_space[i-1][j] == 1:
+                        count +=1
+                    if life_space[i+1][j+1] == 1:
+                        count +=1
+                    if life_space[i+1][j] == 1:
+                        count +=1
+
+                elif j == len(life_space[i]):
+                    if life_space[i][j-1] == 1:
+                        count +=1
+                    if life_space[i-1][j-1] == 1:
+                        count +=1
+                    if life_space[i-1][j] == 1:
+                        count +=1
+                    if life_space[i+1][j-1] == 1:
+                        count +=1
+                    if life_space[i+1][j] == 1:
+                        count +=1
+
+                else:
+                    if life_space[i][j-1] == 1:
+                        count +=1
+                    if life_space[i-1][j-1] == 1:
+                        count +=1
+                    if life_space[i-1][j] == 1:
+                        count +=1
+                    if life_space[i+1][j-1] == 1:
+                        count +=1
+                    if life_space[i+1][j] == 1:
+                        count +=1
+                    if life_space[i][j+1] == 1:
+                        count +=1
+                    if life_space[i-1][j+1] == 1:
+                        count +=1
+                    if life_space[i+1][j+1] == 1:
+                        count +=1
+
+                #checking for new life value
+                if life_space[i][j] == 0:
+                    if count == 3:
+                        new_state[i].append(1)
+                    else:
+                        new_state[i].append(0)
+                else:
+                    if count < 2:
+                        new_state[i].append(0)
+                    elif count > 3:
+                        new_state[i].append(0)
+                    else:
+                        new_state[i].append(1)
+    return new_state
+
+
+
 def print_life(life_space):
     length = len(life_space)
     string = ""
@@ -53,11 +212,13 @@ def print_life(life_space):
 
 
 
-width = 4
-height = 5
+width = 20
+height = 20
 
 test = dead_state(width,height)
 test2 = random_state(width,height)
+print_life(test2)
+test3 = next_state(test2)
+print_life(test3)
 
 print_life(test)
-print_life(test2)
